f9692820bbb009674c9aa65d0bd4d2cde9674102
danis <ahrnadeev@gmail.com>
2024-02-12 06:24:16 +0300

    meow

---
+++ src/main/java/beans/PageError.java
@@ -0,0 +1,14 @@
+package beans;
+
+import jakarta.faces.bean.ManagedBean;
+
+@ManagedBean(name = "pageError", eager = true)
+public class PageError {
+    public PageError() {
+        System.out.println("PageError bean started!");
+    }
+    //  этот класс мне нужен был для тестов :(
+    public String getMessage() {
+        return "Контент страницы-шаблона (сообщите об ошибке, если видите это)\n";
+    }
+}
